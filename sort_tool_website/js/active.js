2bf8f490480ecb223fe2e0bb3ae2017ccad5b0d8
uchida_ryota <133556427+ryota8888@users.noreply.github.com>
2023-11-11 17:18:35 +0900

    Add files via upload

--- sort_tool_website/js/active.js
+++ sort_tool_website/js/active.js
@@ -2,27 +2,41 @@
 // active設定ためのjs
 const nav_list = document.querySelectorAll('.navi .list');
 const check_item = document.querySelectorAll('.check_item');
+const fil_reset = document.querySelector('.filter_modal .reset_btn');
 
+console.log('アクティブjs 接続中');
+// console.log(fil_reset);
 
 const activetionAdd = (item) => {
     item.classList.add('active');
-    
+}
+const allActiveRemove = (list_item) => {
+    console.log('removeしました。');
+    list_item.forEach(item=>{
+        item.classList.remove('active');
+        // これで一度全部removeしている。
+    });
 }
 
 // naviバーのアクティブ設定
 nav_list.forEach(tab => {
     tab.addEventListener('click', () => {
-        nav_list.forEach(tab2=>{
-            tab2.classList.remove('active');
-            // これで一度全部removeしている。
-        });
+        allActiveRemove(nav_list);
         activetionAdd(tab);
     });
 });
 
+// フィルターのアクティブ設定
+
+// アクティブ追加
 check_item.forEach(ch_item => {
     ch_item.addEventListener('click', () => {
         ch_item.classList.toggle('active');
     });
+});
+// リセット
+fil_reset.addEventListener('click',() => {
+    allActiveRemove(check_item);
+    // console.log('クリック');
 });
 
--- sort_tool_website/js/modal.js
+++ sort_tool_website/js/modal.js
@@ -1,1 +1,33 @@
 // モーダル用js
+
+// フィルター
+const fil_open = document.querySelector('.item3_container .filter_icon');
+const fil_modal = document.querySelector('.filter_modal');
+const fil_close = document.querySelector('.filter_modal .close_btn');
+const fil_mask = document.querySelector('.filter_modal .filter_modal_mask');
+
+console.log('モーダルjs 接続中');
+// console.log(fil_open);
+
+const OpenModal = (btn, modal) => {
+    btn.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+}
+
+const CloseModal = (close_btn, modal, mask) => {
+
+    //マスクモーダル(薄暗い部分)をクリックして非表示
+    document.addEventListener('click', (close_bg) => {
+        close_bg.target == mask ? modal.style.display = 'none' : '';
+    })
+    //キャンセルボタンクリックで非表示
+    close_btn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+}
+
+// フィルター
+OpenModal(fil_open,fil_modal);
+CloseModal(fil_close, fil_modal, fil_mask);
