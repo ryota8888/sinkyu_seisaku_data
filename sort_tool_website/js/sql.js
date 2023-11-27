744fb1ee864a072282453dcb040ac6fa8764ee10
uchida_ryota <133556427+ryota8888@users.noreply.github.com>
2023-11-27 22:56:08 +0900

    Add files via upload

--- sort_tool_website/js/sql.js
+++ sort_tool_website/js/sql.js
@@ -1,24 +1,22 @@
 console.log('sql.js接続中');
+const filID =[];
+filID[0]='rarity';
+filID[1]='attack';
+filID[2]='defense';
+filID[3]='class';
+filID[4]='position';
+filID[5]='school';
+
 const c_list = [];
-c_list[0] = document.querySelectorAll('#rarity .check_item');
-c_list[1] = document.querySelectorAll('#attack .check_item');
-c_list[2] = document.querySelectorAll('#defense .check_item');
-c_list[3] = document.querySelectorAll('#class .check_item');
-c_list[4] = document.querySelectorAll('#position .check_item');
-c_list[5] = document.querySelectorAll('#school .check_item');
-
 let c_list_act = [];
-c_list_act[0] = document.querySelectorAll('#rarity .check_item.active');
-c_list_act[1] = document.querySelectorAll('#attack .check_item.active');
-c_list_act[2] = document.querySelectorAll('#defense .check_item.active');
-c_list_act[3] = document.querySelectorAll('#class .check_item.active');
-c_list_act[4] = document.querySelectorAll('#position .check_item.active');
-c_list_act[5] = document.querySelectorAll('#school .check_item.active');
 
-const char_img = document.querySelectorAll('.char_img');
+//各filter_itemの要素を取得
+filID.forEach((id,index) => {
+    c_list[index] = document.querySelectorAll(`#${id} .check_item`);
+    c_list_act[index] = document.querySelectorAll(`#${id} .check_item.active`);
+});
+
 const img_list = document.querySelector('.list_content_bg ul')
-let r_active = document.querySelectorAll('#rarity .check_item.active');
-let a_active = document.querySelectorAll('#attack .check_item.active');
 
 const fetch_api = (data) => {
     url = '../php/data_push.php';
@@ -33,14 +31,14 @@ const fetch_api = (data) => {
         .then(response => response.json())  //データ受信
         .then(res => {
             img_list.innerHTML = '';
-            console.log(res);
+            console.log('--検索結果--');
             res.forEach(item => {
                 img_list.innerHTML += `<li class="char_img img_click ${item.name_alpha}"></li>`;
                 console.log(item.name_alpha);
             });
         })
         .catch(error => {
-            console.log('エラーが発生しました。');
+            img_list.innerHTML = '<li class="message_box"><div class="error_message">問題が発生したため、検索に失敗しました。<br>ページのリロードをお試しください。<br>もしリロードをしても解決しなかった場合、<a href="#">バグ報告タブ</a>にてご報告いただけますと幸いです。</div></li>';
         });
 }
 
@@ -58,22 +56,16 @@ check_item.forEach(fil_item => {
         c_list.forEach(list => {
             request.push(pushArray(list));
         });
-        c_list_act[0] = document.querySelectorAll('#rarity .check_item.active');
-        c_list_act[1] = document.querySelectorAll('#attack .check_item.active');
-        c_list_act[2] = document.querySelectorAll('#defense .check_item.active');
-        c_list_act[3] = document.querySelectorAll('#class .check_item.active');
-        c_list_act[4] = document.querySelectorAll('#position .check_item.active');
-        c_list_act[5] = document.querySelectorAll('#school .check_item.active');
+        filID.forEach((id,index) => {
+            c_list_act[index] = document.querySelectorAll(`#${id} .check_item.active`);
+        });
 
-        for (let i = 0; i < c_list.length; i++) {
-            if (c_list_act[i].length) {  //アクティブがあるときのみ実行
-                request[i] = [];
-                c_list_act[i].forEach(add_item => {
-                    request[i].push(add_item.value);
-                });
+        c_list_act.forEach((actItem,index) => {  //c_list_actの数まわす
+            if(actItem.length){  //アクティブ状態のアイテムがあるときのみ実行
+                request[index]=[];
+                request[index]=pushArray(actItem);  //各actItemのアクティブになっている数まわす
             }
-        }
-        console.log(request[0]);
+        });
         fetch_api(request);
     });
 });
